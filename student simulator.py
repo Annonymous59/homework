d2334db0ab3f3e4366c02d018c50789ff898bbb9
maslo <illamaslovskij7@gmail.com>
2024-03-24 00:22:44 +0200

    Hello

--- student simulator.py
+++ student simulator.py
@@ -1,4 +1,4 @@
-
+import logging
 import time
 
 class Student:
@@ -6,6 +6,8 @@ class Student:
         self.name = name
         self.grade = grade
         self.money = money
+        logging.basicConfig(level=logging.WARNING, filename="logs.log", filemode="w", format="You need to try harder %(asctime)s : %(levelname)s, %(message)s")
+        logging.basicConfig(level=logging.DEBUG, filename="logs.log", filemode="w",format="All great %(asctime)s : %(levelname)s, %(message)s")
     def study(self, hours):
         self.grade += hours * 0.1
 
@@ -18,11 +20,13 @@ class Student:
         self.money += hours * 0.1
     def get_grade(self):
         return self.grade
+        logging.debug("debug")
 
     def check_myself(self):
 
         if self.money <= 0:
             qi = input("Some low money you need to work harder Y/N")
+            logging.warning("Low money")
             if qi.lower() == "y":
                 print("Working...")
                 time.sleep(2)
@@ -32,6 +36,7 @@ class Student:
 
         if self.grade <= 0:
             q = input("low grade you need to study harder Y/N:")
+            logging.warning("Low grade")
             if q.lower() == "y":
                 print("Styding...")
                 time.sleep(2)
